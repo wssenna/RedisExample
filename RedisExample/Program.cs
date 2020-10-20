480a8a3860b016e7fb296f9be629f75f6b3044cb
Avanade <Avanade@WIN-Q5TSUCY4RI.corp.avanade.org>
2020-10-20 10:31:23 -0300

    Add project files.

---
+++ RedisExample/Program.cs
@@ -0,0 +1,32 @@
+﻿using ServiceStack.Redis;
+using System;
+
+namespace RedisExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var Usuario1 = new Usuario
+            {
+                Nome = "Mirele",
+                Cpf = "123456"
+            };
+
+            var Usuario2 = new Usuario
+            {
+                Nome = "Bruna",
+                Cpf = "5461231"
+            };
+
+            using (var redisClient = new RedisClient("localhost:6379"))
+            {
+                redisClient.Set<Usuario>(Usuario1.Id.ToString(), Usuario1);
+
+                redisClient.Set<Usuario>(Usuario2.Id.ToString(), Usuario2);
+            }
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ RedisExample/Usuario.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RedisExample
+{
+    public class Usuario
+    {
+        public Guid Id { get; set; }
+        public string Nome { get; set; }
+        public string Cpf { get; set; }
+
+        public Usuario()
+        {
+            Id = Guid.NewGuid();
+        }
+    }
+}
